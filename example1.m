5dc2116daec27e8af5c3c8d9309994096c4f40af
fcsamp <felipecsamp@hotmail.com>
2018-05-06 15:51:12 -0300

    traduzindo alguns comentários

--- example1.m
+++ example1.m
@@ -1,11 +1,14 @@
-pos_condutores = [[0,29];[2.5,29];[7.0,29];[4.0,25]];   % exemplo para validacao
+% three-phase line with neutral conductor
 
+pos_condutores = [[0,29];[2.5,29];[7.0,29];[4.0,25]];   % distances among the conductors
+
+% conductors id are available in condutors.json file
 id_condutores = [57 57 57 44];  % 57 = 336400 26/7 ACSR; 44 = 4/0 6/1 ACSR
 
 [z_bus, y_bus] = carson(pos_condutores,id_condutores);
 
-z012 = sequence_matrix(z_bus,true)  % linha transposta
-z012 = sequence_matrix(z_bus,false) % linha nao transposta
+z012 = sequence_matrix(z_bus,true)  % transposed line
+z012 = sequence_matrix(z_bus,false) % untransposed line
 
 
 
--- example2.m
+++ example2.m
@@ -1,10 +1,12 @@
-% dois circuitos trifasicos e um condutor neutro
+% line with two three phase circuits and a neutral conductor
 
+% conductors position [x,y]
 pos_condutores = [[0,35];[2.5,35];[7.0,35];[0,33];[2.5,33];[7.0,33];[4.0,29]]; 
 
+% conductors id are available in condutors.json file
 id_condutores = [57 57 57 57 57 57 44];  % 57 = 336400 26/7 ACSR; 44 = 4/0 6/1 ACSR
 
-[z_bus,y_bus] = carson(pos_condutores,id_condutores);
+[z_bus,y_bus] = carson(pos_condutores,id_condutores);   % impedance and admittance matrix
 
-z012 = sequence_matrix(z_bus,true)  % linha transposta
-z012 = sequence_matrix(z_bus,false) % linha nao transposta
+z012 = sequence_matrix(z_bus,true)  % transposed line
+z012 = sequence_matrix(z_bus,false) % untransposed line
